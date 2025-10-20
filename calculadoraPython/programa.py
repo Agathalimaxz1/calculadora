8f8b5c8c0291c6aa2fb3f028d1b1861d35b9edc1
IvensAr7 <cabecinha@gmail.com>
2025-10-20 14:15:36 -0300

    atividade

--- calculadoraPython/programa.py
+++ calculadoraPython/programa.py
@@ -1,4 +1,5 @@
 from operacoes import *
+import emoji
 
 n1 = int(input("Digite o número: "))
 n2 = int(input("Digite o número: "))
@@ -14,9 +15,20 @@ CALCULADORA - HTA DIVONICA
           ''')
     
 while True:
-    num  = input("Escolha uma opção de 1 a 4, e 0 para sair: ")
+    num  = input("Escolha uma opção de 1 a 4 e 0 para sair: ")
     if num == "1":
         print(f'Resultado: {soma(n1, n2)}')
         break
-  
-
+    elif num == "2":
+        print(f"Resultado: {subtracao(n1, n2)}")
+        break  
+    elif num == "3":
+        print(f"Resultado: {multiplicacao(n1, n2)}")
+        break
+    elif num == "4":
+        print(f"Reultado: {divisao(n1, n2)} ")
+        break
+    elif num == "0":
+        print("Programa finalizado")
+        break
+print(emoji.emojize("programa finalizado :red_heart: "))
